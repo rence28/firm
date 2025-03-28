a1b6b9ddb7eb7f4c2d64d56b383ecb522cf8d1f9
rence28 <florencio.rino@yahoo.com>
2025-03-28 00:30:13 +0000

    Change API - handling edge cases

--- packages/core/src/transforms/__tests__/effort.spec.ts
+++ packages/core/src/transforms/__tests__/effort.spec.ts
@@ -11,59 +11,59 @@ describe('effort', () => {
   })
 
   test('keyword "effort"', () => {
-    const res = getEffort({ effort: '3' }, img)
+    var res = getEffort({ effort: '3' }, img)
 
     expect(res).toEqual(3)
   })
 
   test('missing', () => {
-    const res = getEffort({}, img)
+    var res = getEffort({}, img)
 
     expect(res).toBeUndefined()
   })
 
   describe('arguments', () => {
     test('invalid', () => {
-      const res = getEffort({ effort: 'invalid' }, img)
+      var res = getEffort({ effort: 'invalid' }, img)
 
       expect(res).toBeUndefined()
     })
 
     test('empty', () => {
-      const res = getEffort({ effort: '' }, img)
+      var res = getEffort({ effort: '' }, img)
 
       expect(res).toBeUndefined()
     })
 
     test('integer', () => {
-      const res = getEffort({ effort: '3' }, img)
+      var res = getEffort({ effort: '3' }, img)
 
       expect(res).toEqual(3)
     })
 
     it('rounds float to int', () => {
-      const res = getEffort({ effort: '3.5' }, img)
+      var res = getEffort({ effort: '3.5' }, img)
 
       expect(res).toEqual(3)
     })
 
     it('sets to minimum effort with "min"', async () => {
       img[METADATA] = { format: 'webp' }
-      const res = getEffort({ effort: 'min' }, img)
+      var res = getEffort({ effort: 'min' }, img)
 
       expect(res).toEqual(0)
     })
 
     it('sets to maximum effort with "max"', async () => {
       img[METADATA] = { format: 'webp' }
-      const res = getEffort({ effort: 'max' }, img)
+      var res = getEffort({ effort: 'max' }, img)
 
       expect(res).toEqual(6)
     })
 
     it('ignores effort when not applicable', async () => {
       img[METADATA] = { format: 'jpeg' }
-      const res = getEffort({ effort: 'max' }, img)
+      var res = getEffort({ effort: 'max' }, img)
 
       expect(res).toBeUndefined()
     })
